407d445b5f98fc72bf5ea07155341c92626714c2
vikas kumawat <42940952+Vikaskumawat2018@users.noreply.github.com>
2024-09-10 22:19:51 +0545

    Update mail-server.sh

--- hestiacp/mail-server.sh
+++ hestiacp/mail-server.sh
@@ -1,11 +1,15 @@
+apt update
 apt install -y sudo htop
-mkdir test && cd test
-wget -O swap.sh https://raw.githubusercontent.com/advance-things/linux-swap/main/swap.sh
-chmod +x swap.sh
+sudo timedatectl set-timezone Asia/Kolkata
 
+mkdir test && cd test
+
+wget -O swap.sh https://raw.githubusercontent.com/advance-things/linux-swap/main/swap.sh
+
+chmod +x swap.sh
 ./swap.sh -s 2048 -f /swapfile -F true
 
 
 wget https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh
 
-bash hst-install.sh --hostname hcp.durbhasigurukulam.com --email info@durbhasigurukulam.com --password vikaskumawat012356 --apache no --phpfpm no --vsftpd no --sieve yes --clamav no --spamassassin no
+bash hst-install.sh --hostname hcp.durbhasigurukulam.com --email info@durbhasigurukulam.com  --apache no --phpfpm no --vsftpd no --sieve yes --clamav no --spamassassin no
